cee8b9db6a85ce48f4d8efc249824ef9236e1c46
baobabnamu <iwantbaobab@gmail.com>
2022-05-02 20:29:41 +0900

    feat: TypeORM 연결 및 환경 변수 적용 시 누락된 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import emailConfig from './config/emailConfig';
 import { validationSchema } from './config/validationSchema';
 import { EmailModule } from './email/email.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -14,6 +15,16 @@ import { UsersModule } from './users/users.module';
       load: [emailConfig],
       isGlobal: true,
       validationSchema,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: Boolean(process.env.DB_SYNC),
     }),
     EmailModule,
     UsersModule,
