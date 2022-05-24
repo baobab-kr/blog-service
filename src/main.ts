9843990ebcb8527c759a61da6583abe84f72bffc
baobabnamu <iwantbaobab@gmail.com>
2022-05-24 16:22:41 +0900

    fix: cors issue 옵션 추가

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ dotenv.config({
 });
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors:true});
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
   }));
