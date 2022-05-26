29e4ae324686170167bef5c35b0afc78417e0263
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2022-05-26 21:25:48 +0900

    fix: cors origin issue 수정

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,15 @@ dotenv.config({
 });
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors:true});
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
   }));
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.use(cookieParser());
   await app.listen(3000);
 }
