af877fee093bdfac21ba0ee7e60e8e6eced178fc
baobabnamu <iwantbaobab@gmail.com>
2022-05-26 20:25:15 +0900

    feat: cors options add

--- src/main.ts
+++ src/main.ts
@@ -14,11 +14,16 @@ dotenv.config({
 });
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors:true});
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
   }));
   app.use(cookieParser());
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,PUT,PATCH,POST,DELETE',
+    credentials: true,
+});
   await app.listen(3000);
 }
 bootstrap();
